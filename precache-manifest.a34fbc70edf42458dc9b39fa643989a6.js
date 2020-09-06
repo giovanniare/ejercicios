a0683b62bdf863668d96ccaa22245d0f3e6cdd4c
Giovanni <giovanniare9525@gmail.com>
2020-09-06 13:41:51 -0500

    Updates

---
+++ precache-manifest.a34fbc70edf42458dc9b39fa643989a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8139df48a789e8b24500d6f3dde605",
+    "url": "/ejercicios.git/index.html"
+  },
+  {
+    "revision": "13aea13225dbeb494f01",
+    "url": "/ejercicios.git/static/js/2.99511517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ejercicios.git/static/js/2.99511517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5515366ce215a33ef539",
+    "url": "/ejercicios.git/static/js/main.779d9839.chunk.js"
+  },
+  {
+    "revision": "704bc82e0c1523160293",
+    "url": "/ejercicios.git/static/js/runtime-main.8d4e1b1f.js"
+  }
+]);
